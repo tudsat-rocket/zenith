1dfb9c3dd5303a1ab0b7b293103df8ee56a04073
Felix Wiegand <felix-wiegand@protonmail.ch>
2026-05-10 17:59:06 +0200

    Fix tap.sh user environment variable

--- sitl/tap.sh
+++ sitl/tap.sh
@@ -2,7 +2,7 @@ if ip link show tap99 >/dev/null 2>&1; then
     exit 0
 fi
 
-sudo ip tuntap add name tap99 mode tap user $SUDO_USER
+sudo ip tuntap add name tap99 mode tap user $USER
 sudo ip link set tap99 up
 sudo ip addr add 192.168.69.100/24 dev tap99
 sudo ip -6 addr add fe80::100/64 dev tap99
